484119aa2f0e1379b64a9ae4a24f021497fb1c36
dawodahmadi <ahmadi.dawod@gmail.com>
2023-02-19 13:10:23 -0500

    changes to codes

--- src/main/java/com/prog2/labs/LabFive.java
+++ src/main/java/com/prog2/labs/LabFive.java
@@ -20,10 +20,77 @@ public class LabFive {
 	 */
 
 	public boolean isMagicSquare(int[][] matrix) {
-		return false;
-	}
+    int n = matrix.length;
+    int magicSum = n * (n * n + 1) / 2; // Calculate the magic sum
+    
+    // Check the sum of each row
+    for (int i = 0; i < n; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < n; j++) {
+            rowSum += matrix[i][j];
+        }
+        if (rowSum != magicSum) {
+            return false;
+        }
+    }
+    
+    // Check the sum of each column
+    for (int j = 0; j < n; j++) {
+        int colSum = 0;
+        for (int i = 0; i < n; i++) {
+            colSum += matrix[i][j];
+        }
+        if (colSum != magicSum) {
+            return false;
+        }
+    }
+    
+    // Check the sum of the diagonal from top left to bottom right
+    int diag1Sum = 0;
+    for (int i = 0; i < n; i++) {
+        diag1Sum += matrix[i][i];
+    }
+    if (diag1Sum != magicSum) {
+        return false;
+    }
+    
+    // Check the sum of the diagonal from top right to bottom left
+    int diag2Sum = 0;
+    for (int i = 0; i < n; i++) {
+        diag2Sum += matrix[i][n - i - 1];
+    }
+    if (diag2Sum != magicSum) {
+        return false;
+    }
+    
+    // If all checks pass, the square is a magic square
+    return true;
+}
+
+
+
+
+
+
+		
+	
 
 	public int[][] magicSquareGenerator(int arraySize) {
-		return new int[0][0];
-	}
+		int[][] square = new int[arraySize][arraySize];
+                int row = arraySize - 1;
+                int col = arraySize / 2;
+                square[row][col] = 1;
+    
+    for (int i = 2; i <= arraySize * arraySize; i++) {
+        if (square[(row + 1) % arraySize][(col + 1) % arraySize] == 0) {
+            row = (row + 1) % arraySize;
+            col = (col + 1) % arraySize;
+        } else {
+            row = (row - 1 + arraySize) % arraySize;
+        }
+        square[row][col] = i;
+    }
+    
+    return square;
+}
 }
